1034bb2e799a571d3a3a003c8121abeb67f9b7ac
tomeliasi <tomeliasi65@gmail.com>
2025-06-21 16:15:36 +0300

    Fix(App.py , init.py ) fix code format

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from prometheus_client import make_wsgi_app
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from .metrics import REQUEST_COUNTER, AGE_LEGAL_COUNTER
 from .app import main_bp
 
 def create_app():
