354025cac88228082fa2ec422d89fac1b195da63
Nave Mano <nave20022@gmail.com>
2025-06-21 18:42:15 +0300

    change files for metrics

--- app/metrics.py
+++ app/metrics.py
@@ -1,5 +1,14 @@
-from prometheus_client import Counter
+from prometheus_client import Counter, Histogram
 
-REQUEST_COUNTER = Counter("check_age_requests_total", "Total check-age requests")
-AGE_LEGAL_COUNTER = Counter("legal_age_true_total", "Users over 18")
+REQUEST_COUNTER = Counter(
+    "total_requests_total", "Total number of requests received"
+)
+
+AGE_LEGAL_COUNTER = Counter(
+    "legal_age_requests_total", "Number of users over legal age (18+)"
+)
+
+REQUEST_LATENCY = Histogram(
+    "request_latency_seconds", "Time spent processing request"
+)
 
