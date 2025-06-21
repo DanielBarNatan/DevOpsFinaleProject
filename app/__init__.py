cc28c776672174b5a133ee097d44024716621083
tomeliasi <tomeliasi65@gmail.com>
2025-06-21 16:19:58 +0300

    fix(_init_.py) : fix code format

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from prometheus_client import make_wsgi_app
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from .app import main_bp
 
+
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(main_bp)
